ae0c3a08bff11996797182b2d2ee6cca3cdf27bf
mohamed-abdelaziz1 <mohamedzizo.46@gmail.com>
2024-09-13 21:10:21 +0300

    editing variables.tf

--- main.tf
+++ main.tf
@@ -14,15 +14,6 @@ provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 
-
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
-}
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-}
 }
 
 resource "aws_instance" "app_server" {
